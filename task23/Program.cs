0273132d7bab1b6e1b97bd0faf2355b083d1d5ae
AleksandrBrazzers <iforum.uz@gmail.com>
2023-03-05 18:13:22 +0500

    Добавил задание 19,21,23

---
+++ task23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Число\tКуб");
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine($"{i}\t{Math.Pow(i,3)}");
+        }
