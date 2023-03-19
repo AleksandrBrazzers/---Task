e75816b05749309e49eedbdbc5bb0bcfa1293ba1
AleksandrBrazzers <iforum.uz@gmail.com>
2023-03-19 12:19:02 +0500

    task 29

---
+++ task29/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program {
+    static void Main(string[] args) 
+    {
+        int[] arr = { 8, 5, 4, 9, 4, 2, 5, 3 };
+        for (int i = 0; i < arr.Length; i++) 
+        {
+            Console.WriteLine(arr[i]);
+        }
+    }
+}
