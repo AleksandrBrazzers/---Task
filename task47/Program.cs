a07d719f86ce6240ffade3eb06125e971fe7fe1b
AleksandrBrazzers <iforum.uz@gmail.com>
2023-03-19 13:47:49 +0500

    task 47,50,52

---
+++ task47/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите количество строк: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите количество столбцов: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        double[,] array = new double[m, n];
+        Random random = new Random();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                array[i, j] = random.NextDouble();
+            }
+        }
+
+        Console.WriteLine("Двумерный массив случайных вещественных чисел:");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                Console.Write($"{array[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ task50/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array = new int[,] 
+        { 
+            { 1, 4, 7, 2 }, 
+            { 5, 9, 5, 3 }, 
+            { 8, 4, 2, 4 } 
+        }; 
+        Console.Write("Введите номер строки: ");
+        int row = Convert.ToInt32(Console.ReadLine()); 
+
+        Console.Write("Введите номер столбца: ");
+        int column = Convert.ToInt32(Console.ReadLine()); 
+
+        if (row >= 0 && row < array.GetLength(0) && column >= 0 && column < array.GetLength(1))
+        {
+            int element = array[row, column]; 
+            Console.WriteLine("Значение элемента: " + element);
+        }
+        else
+        {
+            Console.WriteLine("Такого элемента нет в массиве");
+        }
+    }
+}
