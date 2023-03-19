1649bb0cd27f717c5ab897e48c68d48a1460d142
AleksandrBrazzers <iforum.uz@gmail.com>
2023-03-19 12:04:34 +0500

    Задание 25,27

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,12 +1,16 @@
 ﻿int number;
  Console.Write("Введите число: ");
         number = Convert.ToInt32(Console.ReadLine());
-        int thirdDigit = number / 100 % 10;
-        if (thirdDigit == 0)
- {
-            Console.WriteLine("Третьей цыфры нет.");
-        }
-        else
         {
-            Console.WriteLine("Третья цифра " + thirdDigit);
-}
+            number = number / 1 % 10;
+            Console.WriteLine("Последняя цифра введенного числа: " + number);
+        }
+//         int thirdDigit = number / 100 % 10;
+//         if (thirdDigit == 0)
+//  {
+//             Console.WriteLine("Третьей цыфры нет.");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Третья цифра " + thirdDigit);
+// }
---
+++ task25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+{
+     Console.WriteLine("Число\tСтепень\tВывод");
+    {
+            Console.WriteLine($"{x}\t{y}\t{Math.Pow(x,y)}");
+        }
+}      
