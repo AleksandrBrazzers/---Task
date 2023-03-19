b7d1d8d874e1056a12f17537375c481aa051699d
AleksandrBrazzers <iforum.uz@gmail.com>
2023-03-19 12:58:17 +0500

    task 41,43

---
+++ task41/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите количество чисел: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int count = 0;
+
+        for (int i = 0; i < size; i++) {
+            Console.Write($"Введите число {i + 1}: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num > 0) {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Количество чисел больше 0: {count}");
+    }
+}
