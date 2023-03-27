77fcbdb0b5ef55997c9335fe3519051b2b2af117
AleksandrBrazzers <iforum.uz@gmail.com>
2023-03-27 11:20:34 +0500

    54,56

---
+++ task54/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] m = new int[,] 
+        { 
+        { 1, 4, 7, 2 },
+        { 5, 9, 2, 3 },
+        { 8, 4, 2, 4 } 
+        };
+        for (int i = 0; i < m.GetLength(0); i++)
+        {
+            int[] row = new int[m.GetLength(1)];
+            for (int j = 0; j < m.GetLength(1); j++)
+            {
+                row[j] = m[i, j];
+            }
+            Array.Sort(row, (a, b) => -1 * a.CompareTo(b));
+            for (int j = 0; j < m.GetLength(1); j++)
+            {
+                m[i, j] = row[j];
+            }
+        }
+
+        for (int i = 0; i < m.GetLength(0); i++)
+        {
+            for (int j = 0; j < m.GetLength(1); j++)
+            {
+                Console.Write(m[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ task56/Program.cs
@@ -0,0 +1,51 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[,] m1 = new int[,] 
+        { 
+            { 2, 4 }, 
+            { 3, 2 } 
+        };
+        int[,] m2 = new int[,] 
+        { 
+            { 3, 4 }, 
+            { 3, 3 } 
+        };
+
+        int[,] result = MultiplyMatrices(m1, m2);
+
+        Console.WriteLine("Результат:");
+        PrintMatrix(result);
+    }
+
+    static int[,] MultiplyMatrices(int[,] m1, int[,] m2)
+    {
+        int[,] result = new int[m1.GetLength(0), m2.GetLength(1)];
+
+        for (int i = 0; i < m1.GetLength(0); i++)
+        {
+            for (int j = 0; j < m2.GetLength(1); j++)
+            {
+                for (int k = 0; k < m1.GetLength(1); k++)
+                {
+                    result[i, j] += m1[i, k] * m2[k, j];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
